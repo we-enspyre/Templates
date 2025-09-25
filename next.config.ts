30fadc423c694ffe9c0248471d2bf942a6f852cf
MHA <98241106+MHA963@users.noreply.github.com>
2025-09-26 01:07:06 +0200

    trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NEXT_PUBLIC_ENV === "prod";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: isProd ? "/Templates" : "",
   assetPrefix: isProd ? "/Templates/" : "",
+  trailingSlash: true
 };
 
 export default nextConfig;
